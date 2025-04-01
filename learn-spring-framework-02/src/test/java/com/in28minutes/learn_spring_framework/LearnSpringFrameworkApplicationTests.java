f75f92ff15da43b62576d9e33eed425a6506ace2
Prajwal <sai.prajwal@knotsolutions.com>
2025-04-01 16:49:37 +0530

    section 11 springboot

--- learn-spring-framework-02/src/test/java/com/in28minutes/learn_spring_framework/LearnSpringFrameworkApplicationTests.java
+++ learn-spring-framework-02/src/test/java/com/in28minutes/learn_spring_framework/LearnSpringFrameworkApplicationTests.java
@@ -3,11 +3,11 @@ package com.in28minutes.learn_spring_framework;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class LearnSpringFrameworkApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
